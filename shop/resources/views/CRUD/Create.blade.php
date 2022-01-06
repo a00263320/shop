0994ff5dec30941290b45381d3250c22d64a25d8
a00263320 <96225240+a00263320@users.noreply.github.com>
2022-01-06 09:03:23 +0800

    整理

--- shop/resources/views/CRUD/Create.blade.php
+++ shop/resources/views/CRUD/Create.blade.php
@@ -7,25 +7,33 @@
     <title>新增</title>
 </head>
 <body>
-    <form name="input" action="/api" method="post">
+    <form name="input" action="/login/api" method="post">
     @csrf
     輸入產品種類
     <input type="text" name="input_class">
     <br>
+    <br>
     輸入產品名稱
     <input type="text" name="input_name">
+    <br>
     <br>
     上傳產品圖片
     <input type="text" name="input_photo">
     <br>
+    <br>
     輸入產品描述
-    <input type="text" name="input_describe">
+    <br>
+    <br>
+    <textarea name="input_describe" cols="30" rows="10"></textarea>
+    <br>
     <br>
     輸入產品數量
     <input type="text" name="input_quantity">
     <br>
+    <br>
     輸入產品價格
     <input type="text" name="input_price">
+    <br>
     <br>
     產品上架/下架
     <input type="text" name="input_state">
--- shop/resources/views/CRUD/Update.blade.php
+++ shop/resources/views/CRUD/Update.blade.php
@@ -10,26 +10,35 @@
     <form name="input" action="/api/{{ $showup ['id']}}" method="post">
     @csrf
     @method('put')
-    更新產品圖片
-    <input type="text" name="input_photo">
+    更改產品圖片
+    <input type="text" name="input_photo" value="{{ $showup->photo }}">
     <br>
-    更新產品種類
-    <input type="text" name="input_class">
     <br>
-    更新產品名稱
-    <input type="text" name="input_name">
+    更改產品種類
+    <input type="text" name="input_class" value="{{ $showup->class }}">
     <br>
-    更新產品描述
-    <input type="text" name="input_describe">
     <br>
-    更新產品數量
-    <input type="text" name="input_quantity">
+    更改產品名稱
+    <input type="text" name="input_name" value="{{ $showup->name }}">
     <br>
-    更新產品價格
-    <input type="text" name="input_price">
     <br>
-    更新產品上/下架
-    <input type="text" name="input_state">
+    更改產品描述
+    <br>
+    <br>
+    <textarea name="input_describe" cols="30" rows="10">{{ $showup->describe }}</textarea>
+    {{-- <input type="text" name="input_describe" value="{{ $showup->describe }}" cols="30" rows="20"> --}}
+    <br>
+    <br>
+    更改產品數量
+    <input type="text" name="input_quantity" value="{{ $showup->quantity }}">
+    <br>
+    <br>
+    更改產品價格
+    <input type="text" name="input_price" value="{{ $showup->price }}">
+    <br>
+    <br>
+    更改產品上/下架
+    <input type="text" name="input_state" value="{{ $showup->state }}">
     <input type="submit" value="確定更新產品資訊">
     </form>
 </body>
